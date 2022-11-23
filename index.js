01103d9c9258aa48cb5f0318b49575717be85f54
aminahagi <amina.hagi@student.moringaschool>
2022-11-23 16:07:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(value){
+    if (value<=400) {
+      return ("This one is on me!")
+    }
+    else if(value < 2500) {
+      return ("I will gladly take your thirty bucks.")
+    }
+    else {
+      return ("No can do.")
+    }
+  }
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+function switchOnCharmFromTip(tip){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return("Thank you so much.")
+      break;
+    case 'not as generous':
+      return("Thank you.")  
+      break;
+    default: 'thanks for everything'
+      return("Bye.")
+      break;
+  }
 }
